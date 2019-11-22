ddef509ac44badb724ea1710b4ef07903f918cd7
shineceo97 <shineceo97@naver.com>
2019-11-22 09:48:13 +0900

    first commit

---
+++ src/dynamic_beat_1/DynamicBeat.java
@@ -0,0 +1,16 @@
+package dynamic_beat_1;
+
+import javax.swing.JFrame;
+
+public class DynamicBeat extends JFrame{
+	
+	public DynamicBeat() {
+		setTitle("Dynamic Beat");
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+}
